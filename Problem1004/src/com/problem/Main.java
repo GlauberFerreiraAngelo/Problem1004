21f93811ffe89829be05dcac7c038fe8503d1b30
GlauberFerreiraAngelo <42098584+GlauberFerreiraAngelo@users.noreply.github.com>
2022-02-21 11:41:31 -0300

    Add files via upload

---
+++ Problem1004/src/com/problem/Main.java
@@ -0,0 +1,16 @@
+package com.problem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int A, B, PROD;
+		
+		A = 3;
+		B = 9;
+		
+		PROD = A * B;
+		
+		System.out.println("PROD = " + PROD);
+	}
+}
